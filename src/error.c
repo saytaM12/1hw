8de995b9a5251740a7e2a1def2781b02f1ee146d
Matyas <matyas.oujezdsky@gmail.com>
2023-03-14 16:50:08 +0100

    a

---
+++ src/error.c
@@ -0,0 +1,7 @@
+// error.c
+// Řešení IJC-DU1, příklad b), 20.3.2023
+// Autor: Matyáš Oujezdský, FIT
+// Přeloženo: clang version 10.0.0-4ubuntu1
+
+void warning(const char *fmt, ...){}
+void error_exit(const char *fmt, ...){}
